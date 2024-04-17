e51f1d5610978a54a3242f75bd934dbca993a079
alessonJuley <alessonabao@gmail.com>
2024-04-17 20:00:21 +1200

    added DotNetDebugging

--- DotNetDebugging/obj/Release/net8.0/DotNetDebugging.AssemblyInfo.cs
+++ DotNetDebugging/obj/Release/net8.0/DotNetDebugging.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5fb3e8c297086598d3724a35b58baa4e020778")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
