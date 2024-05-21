8e59293bd702cc226480e857441e7533f79f5e0b
alessonJuley <alessonabao@gmail.com>
2024-05-22 02:05:16 +1200

    Added Swagger

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41790379d6b62fc57dc1d22f120446d089046c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ee709de27f101849268a28124e69e678548203")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
