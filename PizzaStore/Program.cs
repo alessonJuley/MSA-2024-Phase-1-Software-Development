d86f22f59302b110ce9d4f96d657aa65eb1d5479
alessonJuley <alessonabao@gmail.com>
2024-05-23 21:42:20 +1200

    Created web API template PizzaStore

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,34 +1,6 @@
-// configure to use swagger
-using Microsoft.OpenApi.Models;
-// connect data to routes
-using PizzaStore.DB;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-          c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
-});
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-          app.UseSwagger();
-          app.UseSwaggerUI(c =>
-          {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
-          });
-}
-
 app.MapGet("/", () => "Hello World!");
-
-// set up routes
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
